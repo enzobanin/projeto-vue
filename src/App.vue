983ade308b33b8238ae3317dd786530b4ce75286
enzobanin <e.banin@aluno.ifsp.edu.br>
2025-10-19 16:51:52 -0300

    Comentando os Exemplos

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   import Banner from './components/Banner.vue';
   import Main from './components/Main.vue';
   import Footer from './components/Footer.vue';
+  // import ExemplosPraticos from './components/ExemplosPraticos.vue';
 </script>
 
 <template>
   <Banner></Banner>
   <Main></Main>
   <Footer></Footer>
+  <!-- <ExemplosPraticos></ExemplosPraticos> -->
 </template>
